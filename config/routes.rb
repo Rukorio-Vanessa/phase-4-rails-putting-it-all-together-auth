10aefde1a97c1699a930d09256944ab2cb89b0df
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-28 09:16:35 +0300

    Completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  #users
+  #resources :users
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+
+  #recipes
+  resources :recipes, only: [:index, :create]
+  
+  #sessions
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
